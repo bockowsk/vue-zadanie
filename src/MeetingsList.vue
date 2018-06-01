8bed16317e01b9940aa63a9d8ac67a7b2cedf2c1
Olimp Bockowski <bockowsk@student.agh.edu.pl>
2018-06-01 17:00:53 +0200

    unregister more users

--- src/MeetingsList.vue
+++ src/MeetingsList.vue
@@ -22,7 +22,7 @@
                 </td>
                 <td>
                 	<button v-if="meeting.participants.indexOf(usern)==-1"  class="button button-outline" @click="register(meeting)" :data-id="meeting.name">Zapisz sie</button>
-                	<button v-else  class="button button-outline" @click="unregister(meeting)" :data-id="meeting.name">wypisz sie</button>
+                	<button v-if="meeting.participants.indexOf(usern)!=-1"  class="button button-outline" @click="unregister(meeting)" :data-id="meeting.name">wypisz sie</button>
                 	&nbsp
                 	<button v-if="meeting.participants.length==0" @click="deleteMeeting(meeting)">Usun puste spotkanie</button></td>
             </tr>
